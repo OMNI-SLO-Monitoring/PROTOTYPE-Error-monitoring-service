9eca755ad3862954535c4384e394c3c8c49144ee
Jacky20000 <dao.jackylong@gmail.com>
2020-06-03 11:50:10 +0200

    Custom error types added
    Added custom Error type 'circuit_breaker_error' which will be logged into the Json if a post message with this error level was received.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,30 @@
-import { Module} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 
+//custom error levels for logging into json file
+let customLevels = {
+  levels: {
+    error: 0,
+    circuit_breaker_error: 1,
+    warn: 2,
+    info: 3,
+    http: 4,
+    verbose: 5,
+    debug: 6,
+    silly: 7
+  }
+}
 @Module({
   imports: [
     WinstonModule.forRoot({
       format: winston.format.json(),
+      levels: customLevels.levels,
       transports: [
         new winston.transports.File({
-          filename: 'error-report.json', level: 'error',
+          filename: 'error-report.json',
         }),
         new winston.transports.Console()
       ]
@@ -19,4 +33,4 @@ import * as winston from 'winston';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -7,9 +7,13 @@ export class AppService {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) { }
 
-  //log received message into a json file
+  //log received message into a json file together with its error type
   async getMessage(error: any) {
-    this.logger.log('error', error);
+    if (error.level === 'circuit_breaker_error') {
+      this.logger.log('circuit_breaker_error', error);
+    } else {
+      this.logger.log('error', error);
+    }
   }
 
 }
